426b585fa614e18c33e3efd675dee94a5f2bf2fc
hyunminji <dpqms0123@gmail.com>
2018-12-09 22:35:07 +0900

    프래그먼트 종료 가능하게 변경

--- SE/app/src/main/java/com/example/jang/se/DialogFragmentAddEvent.java
+++ SE/app/src/main/java/com/example/jang/se/DialogFragmentAddEvent.java
@@ -148,7 +148,7 @@ public class DialogFragmentAddEvent extends DialogFragment {
 
                                 if (SeverResponse.equals("Success")) {
 
-                                    //dismiss();
+                                    dismiss();
                                 }
                             }
                         },
